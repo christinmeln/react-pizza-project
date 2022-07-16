41bbd4729ace81d0acf9ac91714c4458294fbd7c
Kristina Melnichenko <101552786+christinmeln@users.noreply.github.com>
2022-07-17 00:29:10 +0600

    CSS modules on
    Added a file with SCC modules for NotFoundBlock

--- src/components/NotFoundBlock/index.jsx
+++ src/components/NotFoundBlock/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import styles from "./NotFoundBlock.module.scss";
+
 const NotFoundBlock = () => {
   return (
-    <div>
+    <div className={styles.root}>
+      <span>😔</span>
       <h1>Ничего не найдено!</h1>
+      <p className={styles.description}>К сожалению, страница отсутствует.</p>
     </div>
   );
 };
