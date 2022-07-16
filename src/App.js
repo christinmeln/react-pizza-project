87e21dbe0af7ab9d71a45b26d6b7124e6bed2f25
Kristina Melnichenko <101552786+christinmeln@users.noreply.github.com>
2022-07-17 01:13:43 +0600

    Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./scss/app.scss";
 import Header from "./components/Header";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -10,7 +11,10 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <NotFound />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
         </div>
       </div>
     </div>
