e8dddc7e10fe40625c0a9a1aee48561de3fb2c90
Kristina Melnichenko <101552786+christinmeln@users.noreply.github.com>
2022-07-18 01:56:07 +0600

    Cart page was created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="*" element={<NotFound />}></Route>
+            <Route path="cart" element={<Cart />}></Route>
           </Routes>
         </div>
       </div>
