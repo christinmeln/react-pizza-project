035f2ce65606346c96725c08371b9fb50e938f6b
Kristina Melnichenko <101552786+christinmeln@users.noreply.github.com>
2022-07-29 00:58:17 +0600

    Started work with Redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux/es/exports";
 
 import "./scss/app.scss";
+
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
